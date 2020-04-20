11c49493c9cc56dd34403fa972558130ef252254
Beth Ruff <beth.ruff2@gmail.com>
2020-04-20 23:20:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,15 +12,12 @@ module FormsLab
     end 
     
     post '/pirates' do
-      params["pirate"]["ships"].each do |ship_data|
-        Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
+      params[:pirate][:ships].each do |ship_data|
+        Ship.new(ship_data[:name], ship_data[:type], ship_data[:booty])
       end
 
-      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:height], params[:pirate][:weight])
 
-      @ships = Ship.all
-      
-      
       @ships = Ship.all
       
       erb :'pirates/show' 
